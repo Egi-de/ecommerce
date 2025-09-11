0e703016c9ec2c7364cf9a308906a03b04d5911d
NdongoziChrisArsene <arsenendongozi@gmail.com>
2025-09-11 11:25:56 +0200

    Resolved merge conflict in models.py

--- homepage/models.py
+++ homepage/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
-<<<<<<< HEAD
-# Create your models here.
-class product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/')
 
-    def __str__(self):
-        return self.name
-=======
 class Product(models.Model):
     title = models.CharField(max_length=100)          # e.g. Bakery & Biscuits
     name = models.CharField(max_length=100)           # e.g. NutriChoice Digestive
@@ -18,6 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)     # better than CharField
     rating = models.FloatField(default=0)             # e.g. 4.5
     reviews = models.IntegerField(default=0)          # e.g. 25
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -43,17 +34,3 @@ class About(models.Model):
 
     def __str__(self):
         return self.email
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> 4b931f5884a3f9125c54b198efce5c0a75694fe4
